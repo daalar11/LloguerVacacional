9ff858c928628db8adc7ff053c29d74a8222a8af
daalar11 <52834541+daalar11@users.noreply.github.com>
2022-02-01 10:13:31 +0100

    return de all propietats

--- backend_public/routes/web.php
+++ backend_public/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PropietatController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/propietat',[PropietatController::class,'listAll']);
 Route::get('/', function () {
     return view('welcome');
 });
