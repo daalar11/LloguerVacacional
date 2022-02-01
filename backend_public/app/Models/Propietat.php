eb3c231d6fe959f3c7fc8465d6d0482086d0cf99
daalar11 <52834541+daalar11@users.noreply.github.com>
2022-02-01 10:13:42 +0100

    return de all propietats

---
+++ backend_public/app/Models/Propietat.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Propietat extends Model
+{
+    protected $table ="PROPIETAT";
+
+    public $timestamps = false;
+
+    protected $primaryKey ='idPROPIETAT';
+}
