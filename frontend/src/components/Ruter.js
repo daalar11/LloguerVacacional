062fdce2e422a3cbcaa91da521fea47121ca5fc3
Aleixfo <afont15182@alumnes.iesmanacor.cat>
2022-02-27 21:52:26 +0100

    login + register

--- frontend/src/components/Ruter.js
+++ frontend/src/components/Ruter.js
@@ -9,12 +9,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';//Css de bootstrap
 // -- IMPORTS REACT ROUTER DOM
 import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 
+import {Container } from 'reactstrap';
+
+
 // -- VIEWS IMPORTS --
 import Home from "../views/Home";
 import Cercarpropietat from "../views/Cercarpropietat";
 import Viewpropietat from "../views/Viewpropietat";
 import Login from "../views/Login";
 import Contact from "../views/Contact";
+import Register from "../views/Register";
+import Reservar from "../views/Reservar";
+import MisReservas from "../views/MisReservas";
 
 class Ruter extends Component {
 
@@ -27,13 +33,13 @@ class Ruter extends Component {
       <Router>
 
         {/* Div principal Applicacio (Div General) */}
-        <div className="App">
+        
 
         {/* Component Menu (HEADER-NAVBAR) */}
         <Menu/>
 
         {/* Content de la pagina (Contingut del MAIN) */}
-        <div className="content mt-5"> {/*Seccio Content*/}
+        <Container className="content mt-5"> {/*Seccio Content*/}
 
           {/* Rutes del Navbar */}
           <Routes>
@@ -43,11 +49,14 @@ class Ruter extends Component {
             <Route path="/viewpropietat" element={<Viewpropietat />} />
             <Route path="/login" element={<Login />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/reservar" element={<Reservar />} />
+            <Route path="/misreservas" element={<MisReservas />} />
             
           </Routes>
           
-        </div>{/* Fi div Content */}
-      </div>{/* Fi div App */}
+        </Container>{/* Fi div Content */}
+     
     </Router>
     );//FI DEL RETURN DEL RENDER
   }//FI DEL RENDER
--- frontend/src/views/Home.js
+++ frontend/src/views/Home.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 
 import 'bootstrap/dist/css/bootstrap.min.css';//Bootstrap
-import {Container} from 'reactstrap';
+import {Row, Col, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+
+import Carrousel from "../components/Carrousel";
+
+import {Link} from "react-router-dom";
 
 class Home extends Component {
 
@@ -9,11 +13,31 @@ class Home extends Component {
 
     return (
       
-      <Container>
+      <Row>
 
-        Pagina Home 
+        <Breadcrumb>
+          <BreadcrumbItem active>
+            Home
+          </BreadcrumbItem>
+        </Breadcrumb>
+
+        <Carrousel />
+        <Col xs="12" className="mt-5">
+
+          Cerca Una Casa!
+          <br></br>
+          <Link to="/cercarpropietat">Cercador</Link>
+
+        </Col>
+        <Col xs="12" className="mt-5">
+
+          Dona d'alta la teva propia propietat i comença a alquilar!
+          <br></br>
+          <a href="http://localhost:8080/">BackOffice!</a>
+
+        </Col>
       
-      </Container>  
+      </Row>  
       
     );
   }
