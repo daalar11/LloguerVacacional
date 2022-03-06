1afb19db6e5c897777a3ef8c580b1396f194f34e
David <david.daa.2000@gmail.com>
2022-03-06 17:29:42 +0100

    calculador de preus en funcio de tarifes

--- frontend/src/views/MisReservas.js
+++ frontend/src/views/MisReservas.js
@@ -7,13 +7,39 @@ import {Link} from "react-router-dom";
 
 //Css de ViewPropietat
 import '../components/PropietatExpand.css';
-
+import axios from 'axios';
 class Home extends Component {
-  
+  constructor(props) {
+    super(props);
+
+      this.state = {
+        reserves: [],
+        isLoading: false,
+        error: null,
+      };
+  }
+
+  getReservesByUsuari = () => {
+    var idClient=localStorage.getItem('idUsuariLogeat');
+    var url = "http://127.0.0.1:8000"
+    var request = "/misReservas/"+idClient;
+    
+    axios.get(url + request)
+    .then(res => {this.setState({
+      reserves: res.data,
+          status: true
+        });
+    })
+    //Tractam errors
+    .catch(error => this.setState({
+      error,
+      isLoading: false
+    }));
+  }
 
   render() {
-
-   
+   console.log(localStorage.getItem('idUsuariLogeat'));
+   console.log(this.state.reserves)
 
     return (
       
